ddebaafc36c433fb3e178800294212e3c3360c03
Bartosz Tertil <bartosz.tertil@external.telekom.de>
2026-03-02 20:11:58 +0100

    more examples for trino comands

--- example.sql
+++ example.sql
@@ -36,3 +36,42 @@ SELECT
     rank() OVER (ORDER BY sum(sales_id) DESC) rank
 FROM iceberg.sales.sales_rep GROUP BY sales_person_name;
 
+-- new table
+
+create table iceberg.sales.sales_rep_ranked as
+    SELECT
+        sales_person_name,
+        count(*) ile,
+        avg(sales_id) avg_score,
+        sum(sales_id) total_score,
+        rank() OVER (ORDER BY sum(sales_id) DESC) rank
+    FROM iceberg.sales.sales_rep GROUP BY sales_person_name;
+
+-- statistics for query optimization
+analyze iceberg.sales.sales_rep_ranked;
+
+SELECT * FROM iceberg.sales.sales_rep_ranked;
+
+-- some examples 
+show catalogs;
+
+show schemas in iceberg;
+show schemas from iceberg;
+
+show tables in iceberg.sales;
+show tables from iceberg.sales;
+
+-- session information
+show session;
+show session like 'dist%';
+-- set session dist_sql_planner_iceberg_optimized_joins = true;
+show roles;
+show current roles;
+
+select 'Today is ' || cast(current_date as varchar);
+
+use iceberg.sales;
+select * from sales_rep;
+
+how columns from sales_rep_ranked;
+
